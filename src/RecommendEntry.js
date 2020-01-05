5e3f5619251f723fef7d6c6a551b14ae1710a7db
Alex Leska <deskmicealex@gmail.com>
2020-01-04 23:14:05 -0600

    Refactor RecommendEntry & Panel to functional components

--- src/RecommendEntry.js
+++ src/RecommendEntry.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-class RecommendEntry extends React.Component {
-  render() {
-    return (
-      <li key={this.props.heroId}>
-        {this.props.name} Winrate:{' '}
-        {(this.props.winrate * 100).toString().substr(0, 4)}%
-      </li>
-    );
-  }
+function RecommendEntry(props) {
+  return (
+    <li key={props.heroId}>
+      {props.name} Winrate: {(props.winrate * 100).toString().substr(0, 4)}%
+    </li>
+  );
 }
 
 export default RecommendEntry;
--- src/RecommendPanel.js
+++ src/RecommendPanel.js
@@ -1,23 +1,20 @@
 import React from 'react';
-// import { css, jsx } from '@emotion/core';
 
 import RecommendEntry from './RecommendEntry';
 
-class RecommendPanel extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.recommendations.map(recommendation => (
-          <RecommendEntry
-            key={recommendation.heroId}
-            heroId={recommendation.heroId}
-            name={recommendation.name}
-            winrate={recommendation.winrate}
-          ></RecommendEntry>
-        ))}
-      </ul>
-    );
-  }
+function RecommendPanel(props) {
+  return (
+    <ul>
+      {props.recommendations.map(recommendation => (
+        <RecommendEntry
+          key={recommendation.heroId}
+          heroId={recommendation.heroId}
+          name={recommendation.name}
+          winrate={recommendation.winrate}
+        ></RecommendEntry>
+      ))}
+    </ul>
+  );
 }
 
 export default RecommendPanel;
