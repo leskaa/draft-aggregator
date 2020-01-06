f171a69c320ae3d1b5493a23cae7c510b3849e9a
Alex Leska <deskmicealex@gmail.com>
2020-01-05 23:46:03 -0600

    Refactor HeroSelector to functional component

--- src/HeroSelector.js
+++ src/HeroSelector.js
@@ -1,64 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 
-class HeroSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: null,
-      isLoading: false,
-      error: null,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function HeroSelector(props) {
+  const [selected, setSelected] = useState(null);
 
-  handleChange(selection) {
-    let team = this.props.team;
+  const styles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px solid pink',
+      color: state.isSelected ? 'red' : 'blue',
+      padding: 100,
+    }),
+    control: () => ({
+      // none of react-select's styles are passed to <Control />
+      width: 200,
+    }),
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+
+      return { ...provided, opacity, transition };
+    },
+  };
+
+  const handleChange = selection => {
+    let team = props.team;
     if (selection == null) {
-      this.props.removeFromTeam(this.state.selected.value, team);
-    } else if (this.state.selected != null) {
-      this.props.removeFromTeam(this.state.selected.value, team);
-      this.props.addToTeam(selection.value, team);
+      props.removeFromTeam(selected.value, team);
+    } else if (selected != null) {
+      props.removeFromTeam(selected.value, team);
+      props.addToTeam(selection.value, team);
     } else {
-      this.props.addToTeam(selection.value, team);
+      props.addToTeam(selection.value, team);
     }
-    this.setState({ selected: selection });
-  }
+    setSelected(selection);
+  };
 
-  render() {
-    const styles = {
-      option: (provided, state) => ({
-        ...provided,
-        borderBottom: '1px solid pink',
-        color: state.isSelected ? 'red' : 'blue',
-        padding: 100,
-      }),
-      control: () => ({
-        // none of react-select's styles are passed to <Control />
-        width: 200,
-      }),
-      singleValue: (provided, state) => {
-        const opacity = state.isDisabled ? 0.5 : 1;
-        const transition = 'opacity 300ms';
-
-        return { ...provided, opacity, transition };
-      },
-    };
-
-    return (
-      <Select
-        style={styles}
-        value={this.state.selected}
-        onChange={this.handleChange}
-        isClearable="true"
-        menuShouldScrollIntoView="true"
-        options={this.props.options.map(option => ({
-          value: option.id,
-          label: option.localized_name,
-        }))}
-      />
-    );
-  }
+  return (
+    <Select
+      style={styles}
+      value={selected}
+      onChange={handleChange}
+      isClearable="true"
+      menuShouldScrollIntoView="true"
+      options={props.options.map(option => ({
+        value: option.id,
+        label: option.localized_name,
+      }))}
+    />
+  );
 }
 
 export default HeroSelector;
