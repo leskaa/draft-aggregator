1abf9d3485074138432f6ec1a77332e55c9fceee
Alex Leska <deskmicealex@gmail.com>
2020-01-07 23:47:02 -0600

    Refactor App component to be a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import array from 'lodash/array';
 
 import HeroSelector from './HeroSelector.js';
@@ -6,26 +6,19 @@ import RecommendPanel from './RecommendPanel.js';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: [],
-      isLoading: false,
-      error: null,
-      allies: [],
-      opponents: [],
-      synergies: [],
-      counters: [],
-      recommendations: [],
-    };
-  }
+function App(props) {
+  const [options, setOptions] = useState([]);
+  const [allies, setAllies] = useState([]);
+  const [opponents, setOpponents] = useState([]);
+  const [synergies, setSynergies] = useState([]);
+  const [counters, setCounters] = useState([]);
+  const [recommendations, setRecommendations] = useState([]);
 
-  renderRecommendations = team => {
-    let teamData = [...this.state.counters, ...this.state.synergies];
+  useEffect(() => {
+    let teamData = [...counters, ...synergies];
     // Render empty list with nothing selected
     if (teamData.length === 0) {
-      this.setState({ recommendations: [] });
+      setRecommendations([]);
       return;
     }
     teamData = teamData.map(matchupSet => ({
@@ -37,15 +30,15 @@ class App extends React.Component {
     }));
     // Remove already picked heroes from matchup data
     let pickedHeroes = [];
-    this.state.allies.forEach(hero => pickedHeroes.push(hero));
-    this.state.opponents.forEach(hero => pickedHeroes.push(hero));
+    allies.forEach(hero => pickedHeroes.push(hero));
+    opponents.forEach(hero => pickedHeroes.push(hero));
     teamData.forEach(matchupSet => {
       matchupSet.matchups = matchupSet.matchups.filter(option => {
         return !pickedHeroes.includes(option.heroId) ? true : false;
       });
       matchupSet.matchups.sort((a, b) => (a.heroId > b.heroId ? 1 : -1));
     });
-    let matchupOptions = this.state.options.filter(option => {
+    let matchupOptions = options.filter(option => {
       return !pickedHeroes.includes(option.id) ? true : false;
     });
     // Calculate the average matchup values
@@ -62,55 +55,33 @@ class App extends React.Component {
       });
     }
     averageTeamData.sort((a, b) => (a.winrate < b.winrate ? 1 : -1));
-    this.setState({ recommendations: averageTeamData });
-  };
+    setRecommendations(averageTeamData);
+  }, [allies, counters, opponents, options, synergies]);
 
   // TODO: Improve selection option logic
   // https://stackoverflow.com/questions/26137309/remove-selected-option-from-another-select-box
-  removeFromTeam = (heroId, team) => {
+  const removeFromTeam = (heroId, team) => {
     console.log('remove: ' + heroId);
     if (team === '0') {
-      this.setState(
-        (state, props) => ({
-          allies: state.allies.filter(ally => ally !== heroId),
-          synergies: state.synergies.filter(
-            matchupSet => matchupSet.hero !== heroId
-          ),
-        }),
-        () => {
-          this.renderRecommendations(team);
-        }
-      );
+      setAllies(allies.filter(ally => ally !== heroId));
+      setSynergies(synergies.filter(matchupSet => matchupSet.hero !== heroId));
     } else if (team === '1') {
-      this.setState(
-        (state, props) => ({
-          opponents: state.opponents.filter(opponent => opponent !== heroId),
-          counters: state.counters.filter(
-            matchupSet => matchupSet.hero !== heroId
-          ),
-        }),
-        () => {
-          this.renderRecommendations(team);
-        }
-      );
+      setOpponents(opponents.filter(opponent => opponent !== heroId));
+      setCounters(counters.filter(matchupSet => matchupSet.hero !== heroId));
     }
   };
 
   // TODO: Add loading to deal with api delay
-  addToTeam = (heroId, team) => {
+  const addToTeam = (heroId, team) => {
     console.log('add: ' + heroId);
     if (team === '0') {
-      this.setState((state, props) => ({
-        allies: [...state.allies, heroId],
-      }));
+      setAllies([...allies, heroId]);
     } else if (team === '1') {
-      this.setState((state, props) => ({
-        opponents: [...state.opponents, heroId],
-      }));
+      setOpponents([...opponents, heroId]);
     }
     // TODO: Using Stratz API
     fetch(
-      `https://api.stratz.com/api/v1/Hero/${heroId}/dryad?take=${this.state.options.length}&rank=5,6,7,8&matchLimit=0`
+      `https://api.stratz.com/api/v1/Hero/${heroId}/dryad?take=${options.length}&rank=5,6,7,8&matchLimit=0`
     )
       .then(response => {
         if (response.ok) {
@@ -127,42 +98,28 @@ class App extends React.Component {
             winrate: 0.5 + matchup.synergy / 200,
           }));
           const uniqueMappedMatchups = array.uniqBy(mappedMatchups, 'hero_id');
-          this.setState(
-            (state, props) => ({
-              synergies: [
-                ...state.synergies,
-                {
-                  hero: heroId,
-                  team: team,
-                  matchups: uniqueMappedMatchups,
-                },
-              ],
-            }),
-            () => {
-              this.renderRecommendations(team);
-            }
-          );
+          setSynergies([
+            ...synergies,
+            {
+              hero: heroId,
+              team: team,
+              matchups: uniqueMappedMatchups,
+            },
+          ]);
         } else if (team === '1') {
           const mappedMatchups = data[0].vs.map(matchup => ({
             hero_id: matchup.heroId2,
             winrate: 0.5 - matchup.synergy / 100,
           }));
           const uniqueMappedMatchups = array.uniqBy(mappedMatchups, 'hero_id');
-          this.setState(
-            (state, props) => ({
-              counters: [
-                ...state.counters,
-                {
-                  hero: heroId,
-                  team: team,
-                  matchups: uniqueMappedMatchups,
-                },
-              ],
-            }),
-            () => {
-              this.renderRecommendations(team);
-            }
-          );
+          setCounters([
+            ...counters,
+            {
+              hero: heroId,
+              team: team,
+              matchups: uniqueMappedMatchups,
+            },
+          ]);
         }
       })
       .catch(error =>
@@ -170,8 +127,7 @@ class App extends React.Component {
       );
   };
 
-  componentDidMount() {
-    // TODO: Loading animation before promise resolves
+  useEffect(() => {
     fetch('https://api.stratz.com/api/v1/Hero')
       .then(response => {
         if (response.ok) {
@@ -181,95 +137,91 @@ class App extends React.Component {
         }
       })
       .then(data =>
-        this.setState({
-          options: Object.values(data).map(option => ({
+        setOptions(
+          Object.values(data).map(option => ({
             id: option.id,
             localized_name: option.displayName,
-          })),
-        })
+          }))
+        )
       )
       .catch(error =>
         console.log('Stratz API hero list fetch failed: ' + error)
       );
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="container">
-        <div className="top-right">
-          <p className="team-title">Allies</p>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="0"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="0"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="0"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="0"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="0"
-          ></HeroSelector>
-        </div>
-        <div className="bottom-right">
-          <p className="team-title">Opponents</p>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="1"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="1"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="1"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="1"
-          ></HeroSelector>
-          <HeroSelector
-            removeFromTeam={this.removeFromTeam}
-            addToTeam={this.addToTeam}
-            options={this.state.options}
-            team="1"
-          ></HeroSelector>
-        </div>
-        <div className="left">
-          <RecommendPanel
-            recommendations={this.state.recommendations}
-          ></RecommendPanel>
-        </div>
+  return (
+    <div className="container">
+      <div className="top-right">
+        <p className="team-title">Allies</p>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="0"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="0"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="0"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="0"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="0"
+        ></HeroSelector>
       </div>
-    );
-  }
+      <div className="bottom-right">
+        <p className="team-title">Opponents</p>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="1"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="1"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="1"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="1"
+        ></HeroSelector>
+        <HeroSelector
+          removeFromTeam={removeFromTeam}
+          addToTeam={addToTeam}
+          options={options}
+          team="1"
+        ></HeroSelector>
+      </div>
+      <div className="left">
+        <RecommendPanel recommendations={recommendations}></RecommendPanel>
+      </div>
+    </div>
+  );
 }
 
 export default App;
